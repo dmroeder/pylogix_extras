bf778c1c0184181ec4b3bd7e60a0a6cb445f7ff8
dmroeder <dmroeder@gmail.com>
2026-04-03 13:14:51 -0700

    Updated getting controller fault code

--- get_fault_code.py
+++ get_fault_code.py
@@ -1,44 +1,24 @@
 import pylogix
-from pylogix.lgx_response import Response
-from struct import pack, unpack_from
+import struct
 
 def get_controller_fault(plc):
     """
     User configurable CIP command.  It is up to you to understand the
     data that will be returned
     
-    returns Response class (.TagName, .Value, .Status
+    returns Staus, major, minor
     """
-    conn = plc.conn.connect(False)
-    if not conn[0]:
-        return Response(None, None, conn[1])
+    ret = plc.Message(0x01, 0x73, 0x01, 0x00)
 
-    cip_service = 0x01
-    cip_service_size = 0x02
-    cip_class_type = 0x20
-    cip_class = 0x73
-    cip_instance_type = 0x24
-    cip_instance = 0x01
-
-    request = pack('<BBBBBB',
-                    cip_service,
-                    cip_service_size,
-                    cip_class_type,
-                    cip_class,
-                    cip_instance_type,
-                    cip_instance)
-
-    status, ret_data = plc.conn.send(request, False)
-
-    if status == 0:
-        data = ret_data[44:]
-        major = unpack_from("<H", data, 20)[0]
-        minor = unpack_from("<H", data, 22)[0]
+    if ret.Status == "Success":
+        data = ret.Value[44:]
+        major = struct.unpack_from("<H", data, 20)[0]
+        minor = struct.unpack_from("<H", data, 22)[0]
     else:
         major = None
         minor = None
 
-    return Response(None, (major, minor), status)
+    return ret.Status, major, minor
 
 
 with pylogix.PLC("192.168.1.10") as comm:
