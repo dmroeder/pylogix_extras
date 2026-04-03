d615cfaa867074fed5a2828e9c262e5f7d8675a9
dmroeder <dmroeder@gmail.com>
2026-04-03 13:53:11 -0700

    Added interface counters

---
+++ interface_counters.py
@@ -0,0 +1,9 @@
+
+import pylogix
+import struct
+
+with pylogix.PLC("192.168.1.10") as comm:
+    ret = comm.Message(0x0e, 0xf6, 0x01, 0x04)
+    data = ret.Value[44:]
+    interface_counters = [struct.unpack_from("<I", data, i*4)[0] for i in range(11)]
+    print(interface_counters)
